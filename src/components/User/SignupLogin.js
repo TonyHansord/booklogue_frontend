714c77869548575637c69cdb2ce9e41fc121dd02
Anthony Hansord <tonyhansorddev@gmail.com>
2023-07-18 22:02:45 +1000

    Completed Login Form

--- src/components/User/SignupLogin.js
+++ src/components/User/SignupLogin.js
@@ -1,22 +1,135 @@
+import { Form } from 'react-bootstrap';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function SignupLogin({ authType, show, handleClose }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+
+    if (authType === 'Sign Up') {
+      fetch('/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    } else {
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+
+    handleClose();
+  };
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{authType}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{authType} Form</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            {authType}
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Cancel
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form noValidate onSubmit={handleSubmit}>
+            {authType === 'Sign Up' ? (
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Enter name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid name.
+                </Form.Control.Feedback>
+              </Form.Group>
+            ) : null}
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid email.
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid password.
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            {authType === 'Sign Up' ? (
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid password.
+                </Form.Control.Feedback>
+              </Form.Group>
+            ) : null}
+
+            <Button variant="secondary" type="submit">
+              {authType}
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Cancel
+            </Button>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
