9a48bb86528792a1beb3da3898ae36d5f3916fcb
Anthony Hansord <tonyhansorddev@gmail.com>
2023-07-20 07:25:29 +1000

    Completed Logout function and added routes

--- src/components/User/UserHome.js
+++ src/components/User/UserHome.js
@@ -1,7 +1,55 @@
+import { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 function UserHome({ userName }) {
+  const [books, setBooks] = useState([]);
+  const [myBooks, setMyBooks] = useState([]);
+
+  useEffect(() => {
+    fetch('/books')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBooks(data);
+      });
+
+    fetch('/me/books')
+      .then((res) => res.json())
+      .then((data) => {
+        setMyBooks(data);
+      });
+  }, []);
+
+  const renderBooks = (books) => {
+    return books.map((book) => {
+      return (
+        <Row key={book.id}>
+          <Col>
+            <p>{book.title}</p>
+          </Col>
+          <Col>
+            <p>{book.author}</p>
+          </Col>
+        </Row>
+      );
+    });
+  };
+
   return (
     <div>
       <h1>Welcome Back! {userName} </h1>
+      <Container>
+        <Row>
+          <Col>
+            <h2>All Books</h2>
+            {renderBooks(books)}
+          </Col>
+          <Col>
+            <h2>My Books</h2>
+            {renderBooks(myBooks)}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
